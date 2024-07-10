6822987f5a92b47335aa3b75864fefa4adb09d34
Pedro Debevere <pedro.debevere@skyline.be>
2024-07-10 10:08:45 +0200

    replaced with legacy project

---
+++ Script_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Script_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Script_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("67CF752E-4C4B-4BDF-8472-7927FC9CE2B9")]
+[assembly: AssemblyVersion("1.0.0.0")]
